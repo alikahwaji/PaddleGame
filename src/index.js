cf5bd368129705a40f44c5e023dd72070f6f2f27
alikahwaji <ali.kahwaji@nmit.ac.nz>
2020-11-05 06:50:38 +1300

    Finished the brickes and fixed the ball object

--- src/index.js
+++ src/index.js
@@ -26,10 +26,13 @@ const ctx = canvas.getContext('2d');
 
 let score = 0 // Setting our score board to zero
 
+const brickRowCount = 9; // Setting 9 bricks on a row
+const brickColumnCount = 5; // Setting 5 bricks on column
+
 // Create ball props
 const ball = {
   x: canvas.width / 2, //Start in the middle
-  x: canvas.height / 2,// Start in the middle
+  y: canvas.height / 2,// Start in the middle
   size: 10, // ball 
   speed: 4, // Animation  speed prop
   dx: 4, // Animation direction 
@@ -45,6 +48,29 @@ const paddle = {
   speed: 8,
   dx: 0 // Only moving on the x-axes 
 };
+
+// Create brick props
+const brickInfo = {
+  w: 70, // bricks sharing same props
+  h: 20,
+  padding: 10,
+  offsetX: 45, // position on the x-axes
+  offsetY: 60, // position on the y-axes
+  visible: true // When hit the brick it will be removed
+};
+
+// Create bricks
+const bricks = []; // init bricks array
+for (let i = 0; i < brickRowCount; i++) { // loop through array row
+  bricks[i] = []; //  Set the row bricks array iteration to an empty array
+  for (let j = 0; j < brickColumnCount; j++) { // loop through array column
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX; // i is the row iteration for each brick
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY; // we are lopping and getting position
+    bricks[i][j] = { x, y, ...brickInfo }; // copy and take the array 2D and give it the values of x,y with the object values
+  }
+}
+
+console.log(bricks)
 
 // Draw ball on canvas - check MDN canvas drawing paths
 function drawBall() {
@@ -64,15 +90,30 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// Draw the score board
 function drawScore() {
   ctx.font = '20px Arial';
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+  bricks.forEach(column => { // For columns
+    column.forEach(brick => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent'; // This will be conditional 
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
 }
 
 function draw() {
   drawBall()
   drawPaddle()
   drawScore()
+  drawBricks()
 }
 
 draw()
