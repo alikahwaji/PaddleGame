29f2ca63c1e2a437371954c8df8b6763926443f7
alikahwaji <ali.kahwaji@nmit.ac.nz>
2020-11-05 07:44:04 +1300

    Finished the Todo list with comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//NOTE
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+ */
 // ANCHOR Show/Close DOM selectors
 const rulesBtn = document.getElementById('rules-btn')
 const closeBtn = document.getElementById('close-btn')
@@ -109,16 +113,142 @@ function drawBricks() {
   });
 }
 
+// REVIEW Move paddle on canvas
+function movePaddle() { // Every time draw  on the canvas with can re-draw with certain element 
+  paddle.x += paddle.dx; // paddle will not move until we use the keyboards events
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) { // entire width of the canvas
+    paddle.x = canvas.width - paddle.w; // minus the paddle width
+  }
+
+  if (paddle.x < 0) { // 0 from the x-axes  and this is for the borders detection 
+    paddle.x = 0;
+  }
+}
+
+// Move ball on canvas
+function moveBall() {
+  ball.x += ball.dx; // append the ball on x-axes
+  ball.y += ball.dy; // append the ball on y-axes
+
+  // Wall collision (right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) { // right and left walls
+    ball.dx *= -1; // ball.dx = ball.dx * -1 the reason we are doing this is to reverse the ball to go the other wall and bounce back
+  }
+
+  // Wall collision (top/bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) { // top and bottom walls 
+    ball.dy *= -1; // the reason we are doing this is to reverse the ball to go the other wall and bounce back and not go outside the canvas
+  }
+
+  // console.log(ball.x, ball.y);
+
+  // Paddle collision
+  if (
+    ball.x - ball.size > paddle.x && // Always to in consideration the object size of ball and this check the left 
+    ball.x + ball.size < paddle.x + paddle.w && // checking the right side
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed; // reverse the ball object and bounce off
+  }
+
+  // Brick collision
+  bricks.forEach(column => { // loop through bricks array
+    column.forEach(brick => {
+      if (brick.visible) { // make sure the bricks are visible
+        if (
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
+        ) {
+          ball.dy *= -1; // bounce off the brick
+          brick.visible = false; // once bounce off the brick make it visible false 
+
+          increaseScore(); // change the status
+        }
+      }
+    });
+  });
+
+  // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) { // if hit bottom wall then lose 
+    showAllBricks(); // redraw bricks 
+    score = 0; // reset score 
+  }
+}
+
+// Increase score
+function increaseScore() {
+  score++; // increment by one
+
+  if (score % (brickRowCount * brickRowCount) === 0) { // check the if there are no bricks if true redraw the brocks wall
+    showAllBricks(); // create bricks wall
+  }
+}
+
+// Make all bricks appear
+function showAllBricks() { // loop through the array of bricks 
+  bricks.forEach(column => { // for column set visible to true 
+    column.forEach(brick => (brick.visible = true));
+  });
+}
+
+
+// REVIEW Setup all the drawing 
 function draw() {
+  // Clear the canvas to make sure ends the event once stop using the keys
+  ctx.clearRect(0,0, canvas.width, canvas.height)
+
   drawBall()
   drawPaddle()
   drawScore()
   drawBricks()
 }
 
-draw()
+// Update canvas drwaing and animation
+function update() {
+  movePaddle();
+  moveBall();
+
+  // Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update() // Calling the update function
+
+// Keydown event
+function keyDown(e) {
+  // console.log(1) for any key
+  //console.log(e.key) To target the arrow key
+  if (e.key === 'Right' || e.key === 'ArrowRight') { //  so cases it refers to right 
+    paddle.dx = paddle.speed; // take and set dx to speed paddle 
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') { // same for the left 
+    paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  // console.log(2)
+  // console.log(e.key) // To target the arrow key
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0; // Stop the key once its up
+  }
+}
 
 
+// Keyboard event handlers up+down
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => 
